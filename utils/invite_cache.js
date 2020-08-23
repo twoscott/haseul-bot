4b2ee9f7ce9b56ac0a2799b09202b277808c4b5b
Scotto <twoscott99@gmail.com>
2020-08-24 00:05:47 +0100

    updated for new fetchVanityData function

--- utils/invite_cache.js
+++ utils/invite_cache.js
@@ -5,35 +5,43 @@ const { checkPermissions, resolveMember } = require("../functions/discord.js");
 let inviteCache = new Map();
 let vanityCache = new Map();
 
-exports.ready = async function() {
-    for (let guild of Client.guilds.cache.array()) {
-        let botMember = await resolveMember(guild, Client.user.id);
-        if (checkPermissions(botMember, ["MANAGE_GUILD"])) {
-            try {
-                let guildInvites = await guild.fetchInvites();
-                inviteCache.set(guild.id, guildInvites || new Map());
-            } catch(e) {
-                inviteCache.set(guild.id, new Map());
-                console.error(e);
-            }
-            try {
-                let vanityInvite = await guild.fetchVanityInvite(); // <fetchVanityInvite> edited discord.js code
-                vanityInvite.url = `https://discord.gg/${vanityInvite.code}`;
-                vanityCache.set(guild.id, vanityInvite);
-            } catch(e) {
-                vanityCache.set(guild.id, null);
-            }
-        } else {
+async function cacheGuildInvites(guild) {
+    let botMember = await resolveMember(guild, Client.user.id);
+    if (checkPermissions(botMember, ["MANAGE_GUILD"])) {
+        try {
+            let guildInvites = await guild.fetchInvites();
+            inviteCache.set(guild.id, guildInvites || new Map());
+        } catch(e) {
             inviteCache.set(guild.id, new Map());
+            console.error(e);
+        }
+        try {
+            let vanityInvite = await guild.fetchVanityData();
+            vanityInvite.url = `https://discord.gg/${vanityInvite.code}`;
+            vanityCache.set(guild.id, vanityInvite);
+        } catch(e) {
             vanityCache.set(guild.id, null);
         }
+    } else {
+        inviteCache.set(guild.id, new Map());
+        vanityCache.set(guild.id, null);
+    }
+}
+
+exports.newGuild = async function(guild) {
+    cacheGuildInvites(guild);
+}
+
+exports.onReady = async function() {
+    for (let guild of Client.guilds.cache.array()) {
+        await cacheGuildInvites(guild);
     }
     console.log(`Cached invites for ${inviteCache.size} servers.`);
 }
 
 exports.resolveUsedInvite = async function(guild) {
-    let currentCache = inviteCache.get(guild.id);
-    let currentVanity = vanityCache.get(guild.id);
+    let currentCache = await inviteCache.get(guild.id);
+    let currentVanity = await vanityCache.get(guild.id);
     let newInvites;
     let newVanity;
 
@@ -41,7 +49,7 @@ exports.resolveUsedInvite = async function(guild) {
     let inviteChanges = 0;
 
     newInvites = await guild.fetchInvites().catch(() => {});
-    if (newInvites && newInvites.size > 0) {
+    if (currentCache && newInvites && newInvites.size > 0) {
         for (let newInvite of newInvites.array()) {
             let currentInvite = currentCache.get(newInvite.code);
             if (currentInvite) {
@@ -56,9 +64,8 @@ exports.resolveUsedInvite = async function(guild) {
         }
     }
 
-    newVanity = await guild.fetchVanityInvite().catch(() => {}); // <fetchVanityInvite> edited discord.js code
-    if (newVanity && inviteChanges < 2) {
-        console.log("Passed vanity check");
+    newVanity = await guild.fetchVanityData().catch(() => {});
+    if (currentVanity && newVanity && inviteChanges < 2) {
         newVanity.url = `https://discord.gg/${newVanity.code}`;
         if (currentVanity) {
             if (currentVanity.uses !== null && newVanity.uses > currentVanity.uses) {
